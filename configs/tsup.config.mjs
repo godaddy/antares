1538830639efc527a6872728f7e5f3c2ab208230
Rick Markins <63809847+rmarkins-godaddy@users.noreply.github.com>
2025-02-24 11:48:00 -0700

    fix(TS): get TS to actually work (#21)
    * fix(TS): get TS to actually work
    
    * chore(node): ensure node@23 is default in the repo
    
    * chore(lockfile): fix lockfile
    
    * chore(package-json): remove git add
    
    * chore: Remove console log from tsup config

--- configs/tsup.config.mjs
+++ configs/tsup.config.mjs
@@ -1,11 +1,14 @@
 import browserslistToEsbuild from 'browserslist-to-esbuild';
+import { spawn } from 'node:child_process';
 
 export const shared = {
   format: ['esm', 'cjs'],
   sourcemap: true,
-  experimentalDts: true,
   clean: true,
   treeshake: true,
   target: browserslistToEsbuild(),
-  external: ['react', 'react-dom']
+  external: ['react', 'react-dom'],
+  onSuccess: function onSuccess() {
+    spawn('npx', ['tsc'], { stdio: 'inherit' });
+  }
 };
