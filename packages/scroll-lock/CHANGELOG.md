2b8e8bba98e4814990326134fe72591f02da0bd8
3rd-Eden <info@3rd-Eden.com>
2025-11-18 15:05:33 +0100

    feat: add scroll-lock

---
+++ packages/scroll-lock/CHANGELOG.md
@@ -0,0 +1,8 @@
+# @bento/scroll-lock
+
+## 0.0.0
+
+### Patch Changes
+
+- Initial implementation of ScrollLock primitive component
+
---
+++ packages/scroll-lock/scroll-lock.stories.tsx
@@ -0,0 +1,20 @@
+import { getMeta, getComponentDocs, getStory } from '@bento/storybook-addon-helpers';
+import { ScrollLock as ScrollLockComponent } from './src/index.tsx';
+import { BasicExample } from './examples/basic.tsx';
+import { ConditionalExample } from './examples/conditional.tsx';
+import { HookExample } from './examples/hook.tsx';
+import { ModalExample } from './examples/modal.tsx';
+
+export default getMeta({
+  title: 'components/ScrollLock'
+});
+
+export const Props = getComponentDocs(ScrollLockComponent);
+
+export const Basic = getStory(BasicExample);
+
+export const Conditional = getStory(ConditionalExample);
+
+export const Hook = getStory(HookExample);
+
+export const Modal = getStory(ModalExample);
