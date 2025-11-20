5d9e2cbcd1f0d744dddcd9ed492f917aa1fb1547
Amanda <amarques@godaddy.com>
2025-11-20 15:34:21 -0800

    fix: inline data attribute values

--- packages/input/src/index.tsx
+++ packages/input/src/index.tsx
@@ -104,12 +104,6 @@ export const Input = withSlots<InputProps>(
   React.forwardRef<HTMLInputElement, InputProps>(function Input(args: InputProps, ref) {
     const { props, apply } = useProps(args);
     const { disabled, readOnly, type, required, inputPropRef, value, checked, autoFocus } = props;
-    const isInvalid = !!props['aria-invalid'] && props['aria-invalid'] !== 'false';
-    const isDisabled = disabled || false;
-    const isReadOnly = readOnly || false;
-    const isRequired = required || false;
-    const isEmpty = value === '' || value === undefined || value === null;
-    const isChecked = type === 'checkbox' || type === 'radio' ? !!checked : undefined;
 
     const inputRef = useObjectRef(useMemo(() => mergeRefs(ref, inputPropRef), [ref, inputPropRef]));
     const { isFocused, isFocusVisible, focusProps } = useFocusRing({
@@ -122,17 +116,16 @@ export const Input = withSlots<InputProps>(
       <input
         {...apply({ ...mergeProps(props, focusProps, hoverProps) })}
         ref={inputRef}
-        aria-invalid={isInvalid}
         {...useDataAttributes({
           focused: isFocused,
-          disabled: isDisabled,
           hovered: isHovered,
           focusVisible: isFocusVisible,
-          invalid: isInvalid,
-          readonly: isReadOnly,
-          required: isRequired,
-          empty: isEmpty,
-          checked: isChecked
+          disabled: disabled || false,
+          invalid: !!props['aria-invalid'] && props['aria-invalid'] !== 'false',
+          readonly: readOnly || false,
+          required: required || false,
+          empty: value === '' || value === undefined || value === null,
+          checked: type === 'checkbox' || type === 'radio' ? !!checked : undefined
         })}
       />
     );
