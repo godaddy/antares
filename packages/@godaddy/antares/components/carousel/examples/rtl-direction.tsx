243b04c3e398296743f170f34558ad2125a44167
rmojica-godaddy <rmojica@godaddy.com>
2026-04-15 16:27:24 -0500

    feat: use browser layout direction for RTL (no rtl/dir props) (#155)
    * feat: use browser layout direction for RTL (no rtl/dir props)
    
    * refactor: replace RtlI18nProvider with RTLProvider in carousel and chart components

--- packages/@godaddy/antares/components/carousel/examples/rtl-direction.tsx
+++ packages/@godaddy/antares/components/carousel/examples/rtl-direction.tsx
@@ -1,13 +1,16 @@
 import { Carousel, Flex } from '@godaddy/antares';
+import { RTLProvider } from '../../../utils/rtl-locale-provider.tsx';
 
 export function RTLDirectionExample() {
   return (
-    <Carousel style={{ maxWidth: 400 }} dir="rtl">
-      {Array.from({ length: 3 }).map((_, index) => (
-        <Flex key={index} alignItems="center" justifyContent="center" style={{ height: 300, background: 'lavender' }}>
-          Slide {index + 1}
-        </Flex>
-      ))}
-    </Carousel>
+    <RTLProvider>
+      <Carousel style={{ maxWidth: 400 }}>
+        {Array.from({ length: 3 }).map((_, index) => (
+          <Flex key={index} alignItems="center" justifyContent="center" style={{ height: 300, background: 'lavender' }}>
+            Slide {index + 1}
+          </Flex>
+        ))}
+      </Carousel>
+    </RTLProvider>
   );
 }
