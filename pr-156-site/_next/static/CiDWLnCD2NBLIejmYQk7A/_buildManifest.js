af15f511a133564f5b5d96fb6634f4ba526384da
rmojica-godaddy <rmojica-godaddy@users.noreply.github.com>
2026-04-21 18:18:44 +0000

    Deploying to gh-pages from @ godaddy/antares@c1c1692eb77ce3e9b8296c17a5a79978898c7a5b 🚀

---
+++ pr-156-site/_next/static/CiDWLnCD2NBLIejmYQk7A/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/pr-156-site//_next/:path+",
+        "destination": "/pr-156-site/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
