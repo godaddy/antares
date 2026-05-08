39c47c43a1aef3d30367f2d17ee7b5eea671eb1b
Erwin Gaitan <egaitan@godaddy.com>
2026-05-08 17:08:35 -0500

    chore: preloading minus and plus icons

--- packages/@godaddy/antares/components/number-field/test/number-field.visual.test.tsx
+++ packages/@godaddy/antares/components/number-field/test/number-field.visual.test.tsx
@@ -1,5 +1,6 @@
-import { describe, expect, it } from 'vitest';
+import { beforeAll, describe, expect, it } from 'vitest';
 import { render } from 'vitest-browser-react';
+import { set } from '#components/icon';
 import { NumberFieldBasicExample } from '../examples/basic.tsx';
 import { NumberFieldControlledExample } from '../examples/controlled.tsx';
 import { NumberFieldInvalidExample } from '../examples/invalid.tsx';
@@ -9,6 +10,25 @@ import { NumberFieldValueScaleExample } from '../examples/value-scale.tsx';
 import { NumberFieldFormatOptionsExample } from '../examples/format-options.tsx';
 
 describe('@godaddy/antares', function antares() {
+  beforeAll(function setupIcons() {
+    // Preload the icons used by the checkbox component in case they are not already loaded
+    set({
+      minus: (
+        <svg fill="none" height="24" width="24">
+          <path d="M20 12.75H4a.75.75 0 1 1 0-1.5h16a.75.75 0 1 1 0 1.5z" fill="#111" />
+        </svg>
+      ),
+      plus: (
+        <svg fill="none" height="24" width="24">
+          <path
+            d="M20 11.25h-7.25V4a.75.75 0 1 0-1.5 0v7.25H4a.75.75 0 1 0 0 1.5h7.25V20a.75.75 0 1 0 1.5 0v-7.25H20a.75.75 0 1 0 0-1.5z"
+            fill="#111"
+          />
+        </svg>
+      )
+    });
+  });
+
   describe('#NumberField', function numberFieldTests() {
     it('basic example', async function basicRender() {
       const { container } = await render(<NumberFieldBasicExample />);
