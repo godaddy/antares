0dbd9019a57a116fb62b2d38caba6818acc198c0
egaitan-godaddy <egaitan-godaddy@users.noreply.github.com>
2026-05-11 16:25:58 +0000

    Deploying to gh-pages from @ godaddy/antares@b8378ab4ef2aac2c8dae276489f0403a963a7ba7 🚀

---
+++ pr-145-site/_next/static/AYfdOpBk40W4aC4YT1zG_/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/antares/pr-145-site//_next/:path+",
+        "destination": "/antares/pr-145-site/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
