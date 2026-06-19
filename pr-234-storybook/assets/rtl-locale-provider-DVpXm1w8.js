9082f04f464452724dea7a1cf3f6d131e5e6422e
egaitan-godaddy <egaitan-godaddy@users.noreply.github.com>
2026-06-19 14:40:05 +0000

    Deploying to gh-pages from @ godaddy/antares@255caaa07709ce2ce1061db4a5ff03c932b75f74 🚀

---
+++ pr-234-storybook/assets/rtl-locale-provider-DVpXm1w8.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-DOdH0sfz.js";import{y as t}from"./iframe-wBcvNeCk.js";import{dn as n}from"./useTooltipTrigger-DuBNTP4P.js";import{t as r}from"./exports-DeU2EdmU.js";function i({children:e}){return(0,a.jsx)(n,{locale:o,children:e})}var a,o,s=e((()=>{r(),a=t(),o=`ar-EG`}));export{s as n,i as t};
