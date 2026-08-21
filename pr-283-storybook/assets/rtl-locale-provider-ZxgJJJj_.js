915a4ceef9a207cfe166ac08c41b6efbe15dd684
rmojica-godaddy <rmojica-godaddy@users.noreply.github.com>
2026-08-21 21:23:51 +0000

    Deploying to gh-pages from @ godaddy/antares@9aedfee0137b7e199ebc199ca277def7738680ff 🚀

---
+++ pr-283-storybook/assets/rtl-locale-provider-ZxgJJJj_.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-DEA0MoIu.js";import{F as t}from"./iframe-CvQKHh9S.js";import{Tr as n}from"./useTooltipTrigger-BXNwby16.js";import{t as r}from"./exports-UsAWhR24.js";function i({children:e}){return(0,a.jsx)(n,{locale:o,children:e})}var a,o,s=e((()=>{r(),a=t(),o=`ar-EG`}));export{s as n,i as t};
